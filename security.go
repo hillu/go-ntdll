da2df1a5fc26b6f5d0c806168fb91c00528824bd
Adele Reed <adreed@microsoft.com>
2021-09-21 14:42:22 -0700

    Add code generation for Nt{Query,Set}SecurityObject

--- security.go
+++ security.go
@@ -1,7 +1,34 @@
 package ntdll
 
+import (
+	"golang.org/x/sys/windows"
+)
+
 //go:generate -command mkcode go run mkcode.go --
 //go:generate mkcode $GOFILE
+
+/*
+func:
+NTSTATUS NtQuerySecurityObject(
+	HANDLE 				 Handle,
+	SECURITY_INFORMATION SecurityInformation,
+	PSECURITY_DESCRIPTOR SecurityDescriptor,
+	ULONG				 Length,
+	PULONG				 LengthNeeded
+);
+ */
+
+/*
+func:
+NTSTATUS NtSetSecurityObject(
+	HANDLE Handle,
+	SECURITY_INFORMATION SecurityInformation,
+	PSECURITY_DESCRIPTOR SecurityDescriptor
+);
+ */
+
+
+type SecurityInformationT windows.SECURITY_INFORMATION
 
 /*
 type:
