f8f469b2c728eb8cd3d17d47d97ebe3e12671b93
nodauf <nodauf@krutt.org>
2021-12-14 15:26:08 +0100

    Add NtCreateSection

--- object.go
+++ object.go
@@ -91,6 +91,19 @@ NTSTATUS NtDuplicateObject (
 */
 
 /*
+func:
+NTSTATUS NtCreateSection (
+  _Out_ HANDLE SectionHandle,
+  _In_ ACCESS_MASK DesiredAccess,
+  _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+  _In_opt_ PLARGE_INTEGER MaximumSize,
+  _In_ ULONG SectionPageProtection,
+  _In_ ULONG AllocationAttributes,
+  _In_opt_ HANDLE FileHandle
+);
+*/
+
+/*
 type:
 typedef struct _OBJECT_ATTRIBUTES {
   ULONG           Length;
