c8ea7417426f873ae1b274cd42b46d82d0dd3774
Hilko Bengen <bengen@hilluzination.de>
2022-07-25 18:19:55 +0200

    Add various MEM_* constants

--- object.go
+++ object.go
@@ -325,5 +325,23 @@ const (
 	SEC_RESERVE   = 0x04000000
 	SEC_COMMIT    = 0x08000000
 	SEC_NOCACHE   = 0x10000000
-	MEM_IMAGE     = SEC_IMAGE
+
+	MEM_IMAGE                   = SEC_IMAGE
+	MEM_COMMIT                  = 0x00001000
+	MEM_RESERVE                 = 0x00002000
+	MEM_DECOMMIT                = 0x00004000
+	MEM_RELEASE                 = 0x00008000
+	MEM_FREE                    = 0x00010000
+	MEM_PRIVATE                 = 0x00020000
+	MEM_MAPPED                  = 0x00040000
+	MEM_RESET                   = 0x00080000
+	MEM_TOP_DOWN                = 0x00100000
+	MEM_WRITE_WATCH             = 0x00200000
+	MEM_PHYSICAL                = 0x00400000
+	MEM_ROTATE                  = 0x00800000
+	MEM_DIFFERENT_IMAGE_BASE_OK = 0x00800000
+	MEM_RESET_UNDO              = 0x01000000
+	MEM_LARGE_PAGES             = 0x20000000
+	MEM_4MB_PAGES               = 0x80000000
+	MEM_64K_PAGES               = (MEM_LARGE_PAGES | MEM_PHYSICAL)
 )
