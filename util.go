5872871d38ca011615fed3a8d0a2c9ba4c741c42
Hilko Bengen <bengen@hilluzination.de>
2022-07-28 14:58:40 +0200

    Improve CallWithExpandingBuffer documentation

--- util.go
+++ util.go
@@ -5,27 +5,33 @@ import (
 	"unsafe"
 )
 
-// CallWithExpandingBuffer calls fn which encapsulates the actual API
-// call using buf and resultLength. If fn returns a status that
-// indicates a too small buffer, the buffer is expanded and the call
-// retried, until it succeeds (or fails for another reason).
+// CallWithExpandingBuffer is intended to more seamlessly dealing with
+// API calls that fill a caller-provided buffer with data. Usually,
+// these functions expect the address and current size of the buffer
+// and a reference to which the required buffer size will be written.
 //
-// If the API call succeeds, the buffer size is adjusted according to
-// resultLength.
+// This function calls fn which encapsulates the actual API call using
+// buf and resultLength. If fn returns an NtStatus value that
+// indicates an insufficient buffer sizze, the buffer is resized and
+// the call is retried. This happens until the call succeeds or fails
+// for a non-buffer-related reason.
+//
+// The size of buf is adjusted according to resultLength.
 //
 // Example:
 /*
+var keyName string
 var buf []byte
-var rlen uint32
+var requiredLength uint32
 if st := CallWithExpandingBuffer(func() NtStatus {
-    return NtQueryKey(
-		h, KeyFullInformation,
-		&buf[0],
-		uint32(len(buf)),
-		&rlen,
-	)
-}, &buf, &rlen); st.IsError() {
-	...
+	return NtQueryKey(h, KeyNameInformation, &buf[0], uint32(len(buf)), &requiredLength)
+}, &buf, &requiredLength); st.IsError() {
+	// handle non buffer-size-related error
+} else {
+	kbi := (*KeyNameInformationT)(unsafe.Pointer(&buf[0]))
+	keyName = NewUnicodeStringFromBuffer(
+		(*uint16)(unsafe.Pointer(&kbi.Name)),
+		int(kbi.NameLength)).String()
 }
 */
 func CallWithExpandingBuffer(fn func() NtStatus, buf *[]byte, resultLength *uint32) NtStatus {
